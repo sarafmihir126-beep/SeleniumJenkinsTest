7b7b059f1cb5be944d87615a0ef85f37e5581727
sarafmihir126-beep <sarafmihir126@gmail.com>
2026-03-24 14:50:20 +0530

    Update NewTest.java

--- src/test/java/example/NewTest.java
+++ src/test/java/example/NewTest.java
@@ -1,40 +1,46 @@
-<project xmlns="http://maven.apache.org/POM/4.0.0"
- xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
- xsi:schemaLocation="http://maven.apache.org/POM/4.0.0
- http://maven.apache.org/xsd/maven-4.0.0.xsd">
-<modelVersion>4.0.0</modelVersion>
-<groupId>example</groupId>
-<artifactId>SeleniumJenkinsTest</artifactId>
-<version>1.0</version>
-<dependencies>
-<dependency>
-<groupId>org.seleniumhq.selenium</groupId>
-<artifactId>selenium-java</artifactId>
-<version>4.21.0</version>
-</dependency> 
-<dependency>
-<groupId>org.testng</groupId>
-<artifactId>testng</artifactId>
-<version>7.10.1</version>
-<scope>test</scope>
-</dependency>
-</dependencies>
-<build>
-<plugins>
-<plugin>
-<groupId>org.apache.maven.plugins</groupId>
-<artifactId>maven-compiler-plugin</artifactId>
-<version>3.11.0</version>
-<configuration>
-<source>21</source>
-<target>21</target>
-</configuration>
-</plugin>
-<plugin>
-<groupId>org.apache.maven.plugins</groupId>
-<artifactId>maven-surefire-plugin</artifactId>
-<version>3.2.5</version>
-</plugin>
-</plugins>
-</build>
-</project>
+package example;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.*;
+public class NewTest {
+WebDriver driver;
+@BeforeTest
+public void setup(){
+System.out.println("=================================");
+System.out.println("Starting Selenium Test Execution");
+System.out.println("================================="); 
+String os = System.getProperty("os.name").toLowerCase();
+System.out.println("Detected OS: " + os);
+ChromeOptions options = new ChromeOptions();
+if (os.contains("linux")) {
+options.addArguments("--headless=new");
+options.addArguments("--no-sandbox");
+options.addArguments("--disable-dev-shm-usage");
+System.out.println("Running in headless mode");
+} else {
+System.out.println("Running in normal mode");
+}
+driver = new ChromeDriver(options);
+}
+@Test
+public void testTitle() throws InterruptedException {
+System.out.println("Opening website: https://www.rcpit.ac.in");
+driver.get("https://www.rcpit.ac.in");
+String title = driver.getTitle();
+System.out.println("Page title: " + title);
+Assert.assertTrue(title.toLowerCase().contains("rcpit") || title.length() > 0);
+System.out.println("Title verification PASSED");
+System.out.println("Waiting 10 seconds...");
+Thread.sleep(10000);
+}
+@AfterTest
+public void close(){
+System.out.println("Closing browser...");
+driver.quit();
+System.out.println("=================================");
+System.out.println("Execution Finished");
+System.out.println("=================================");
+}
+} 
